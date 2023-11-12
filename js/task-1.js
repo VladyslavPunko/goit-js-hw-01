78299658971e5023b227fc42811522f60bc03649
VladyslavPunko <143653897+VladyslavPunko@users.noreply.github.com>
2023-11-12 17:24:20 +0100

    fix

--- js/task-1.js
+++ js/task-1.js
@@ -1,9 +1,6 @@
-
-
-function makeTransaction (quantity, pricePerDroid ){
-    totalPrice = quantity * pricePerDroid
-return `You ordered ${quantity} droids worth ${totalPrice} credits!`
-
+function makeTransaction(quantity, pricePerDroid) {
+  const totalPrice = quantity * pricePerDroid;
+  return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
 }
 
 console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
